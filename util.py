d8c169b170c11ba635b0474565fff9827cc6efb2
pat.roe <pat.roe@rotationsolutions.com.au>
2022-05-06 16:45:16 +0800

    Add helper functions and images

---
+++ util.py
@@ -0,0 +1,139 @@
+# Utility functions for management of Drawings and Camo Worms
+# Courtesy of Cara Macnish for CITS4404 2022
+
+import numpy as np
+import imageio
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.path as mpath
+import matplotlib.patches as mpatches
+import matplotlib.bezier as mbezier
+from sklearn.metrics.pairwise import euclidean_distances
+
+rng = np.random.default_rng()
+Path = mpath.Path
+mpl.rcParams['figure.dpi'] = 72 #size of images
+
+# Global variables
+
+IMAGE_DIR = 'images'
+IMAGE_NAME='original'
+MASK = [320, 560, 160, 880] # ymin ymax xmin xmax
+
+# ============================ Image Manipulation ==============================
+
+def crop (image, mask):
+    h, w = np.shape(image)
+    return image[max(mask[0],0):min(mask[1],h), max(mask[2],0):min(mask[3],w)]
+
+def prep_image (imdir, imname, mask):
+    print("Image name (shape) (intensity max, min, mean, std)\n")
+    image = np.flipud(crop(imageio.imread(imdir+'/'+imname+".png"), mask))
+    print("{} {} ({}, {}, {}, {})".format(imname, np.shape(image), np.max(image), np.min(image), round(np.mean(image),1), round(np.std(image),1)))
+    plt.imshow(image, vmin=0, vmax=255, cmap='gray', origin='lower') # use vmin and vmax to stop imshow from scaling
+    plt.show()
+    return image
+
+# ======================== Camo Worm Class Definition ==========================
+
+class Camo_Worm:
+    def __init__(self, x, y, r, theta, deviation_r, deviation_gamma, width, colour):
+        self.x = x
+        self.y = y
+        self.r = r
+        self.theta = theta
+        self.dr = deviation_r
+        self.dgamma = deviation_gamma
+        self.width = width
+        self.colour = colour
+        p0 = [self.x - self.r * np.cos(self.theta), self.y - self.r * np.sin(self.theta)]
+        p2 = [self.x + self.r * np.cos(self.theta), self.y + self.r * np.sin(self.theta)]
+        p1 = [self.x + self.dr * np.cos(self.theta+self.dgamma), self.y + self.dr * np.sin(self.theta+self.dgamma)]
+        self.bezier = mbezier.BezierSegment(np.array([p0, p1,p2]))
+
+    def control_points (self):
+        return self.bezier.control_points
+
+    def path (self):
+        return mpath.Path(self.control_points(), [Path.MOVETO, Path.CURVE3, Path.CURVE3])
+
+    def patch (self):
+        return mpatches.PathPatch(self.path(), fc='None', ec=str(self.colour), lw=self.width, capstyle='round')
+
+    def intermediate_points (self, intervals=None):
+        if intervals is None:
+            intervals = max(3, int(np.ceil(self.r/8)))
+        return self.bezier.point_at_t(np.linspace(0,1,intervals))
+
+    def approx_length (self):
+        intermediates = intermediate_points(self)
+        eds = euclidean_distances(intermediates,intermediates)
+        return np.sum(np.diag(eds,1))
+
+    def colour_at_t(self, t, image):
+        intermediates = np.int64(np.round(np.array(self.bezier.point_at_t(t)).reshape(-1,2)))
+        colours = [image[point[0],point[1]] for point in intermediates]
+        return(np.array(colours)/255)
+
+# ========================= Drawing Class Definition ===========================
+
+class Drawing:
+    def __init__ (self, image):
+        self.fig, self.ax = plt.subplots()
+        self.image = image
+        self.im = self.ax.imshow(self.image, cmap='gray', origin='lower')
+
+    def add_patches(self, patches):
+        try:
+            for patch in patches:
+                self.ax.add_patch(patch)
+        except TypeError:
+            self.ax.add_patch(patches)
+
+    def add_dots(self, points, radius=4, **kwargs):
+        try:
+            for point in points:
+                self.ax.add_patch(mpatches.Circle((point[0],point[1]), radius, **kwargs))
+        except TypeError:
+            self.ax.add_patch(mpatches.Circle((points[0],points[1]), radius, **kwargs))
+
+    def add_worms(self, worms):
+        try:
+            self.add_patches([w.patch() for w in worms])
+        except TypeError:
+            self.add_patches([worms.patch()])
+
+    def show(self, save=None):
+        if save is not None:
+            plt.savefig(save)
+        plt.show()
+
+# ========================= Worm Generation Helpers ============================
+
+# Example of a random worm. You may do this differently.
+
+    # centre points, angles and colour chosen from uniform distributions
+    # lengths chosen from normal distributions with two std parameters passed
+    # width chosen from gamma distribution with shape parameter 3 and scale passed
+
+def random_worm (imshape, init_params):
+    (radius_std, deviation_std, width_theta) = init_params
+    (ylim, xlim) = imshape
+    midx = xlim * rng.random()
+    midy = ylim * rng.random()
+    r = radius_std * np.abs(rng.standard_normal())
+    theta = rng.random() * np.pi
+    dr = deviation_std * np.abs(rng.standard_normal())
+    dgamma = rng.random() * np.pi
+    colour = rng.random()
+    width = width_theta * rng.standard_gamma(3)
+    return Camo_Worm(midx, midy, r, theta, dr, dgamma, width, colour)
+
+# Initialise a random clew
+
+def initialise_clew (size, imshape, init_params):
+    clew = []
+    for i in range(size):
+        clew.append(random_worm(imshape, init_params))
+    return clew
