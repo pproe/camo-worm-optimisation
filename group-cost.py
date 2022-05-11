d27d110e64d71298b2f4630a76495ec54667ea9a
reagantrac <reagantrac@gmail.com>
2022-05-11 14:34:13 +0800

    Basic group cost function
    Calculates intersection of worm control point segments

---
+++ group-cost.py
@@ -0,0 +1,23 @@
+def intersect(a1, a2, b1, b2):
+    area = lambda p1, p2, p3 : (p2[0]-p1[0])*(p3[1]-p1[1])-(p3[0]-p1[0])*(p2[1]-p1[1])
+    v1 = area(a1, a2, b1)/1000
+    v2 = area(a1, a2, b2)/1000
+    return v1 * v2
+
+def worm_segments_intersect(clew):
+    costs = []
+    for worm in clew:
+        min_cost = 10000
+        for worm2 in clew:
+            if worm == worm2: continue
+            points1 = worm.control_points()
+            points2 = worm2.control_points()
+            
+            for i in range(len(points1)-1):
+                cost1 = intersect(points1[i], points1[i+1], points2[i], points2[i+1])
+                cost2 = intersect(points2[i], points2[i+1], points1[i], points1[i+1])
+                cost = max([cost1, cost2])
+                if cost < min_cost:
+                    min_cost = cost
+        costs.append(min_cost)
+    return costs
