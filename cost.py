df702c7ad3a998c88289de564101d70a109ace0b
RupesAUS <rupert@LAPTOP-HVUOJM2M.localdomain>
2022-05-12 21:12:40 +0800

    Added basic width cost function with static parameter. Renamed group-cost.py to groupcost.py due to import error

--- cost.py
+++ cost.py
@@ -1,10 +1,16 @@
+from groupcost import *
+from internalcost import *
+
 # Base Class to take in a clew of worms and return a cost value
 
 class CostFunction:
     # Static Class Variables to represent Weight Parameters for:
-    w_i = -1 # Internal knowledge
-    w_g = -1 # Group knowledge
-    w_e = -1 # External knowledge
+    w_i = 1 # Internal knowledge
+    w_g = 1 # Group knowledge
+    w_e = 1 # External knowledge
+
+    # Extra Static Variables ...
+    max_possible_width = 720
 
     def __init__(self, clew):
         self.clew = clew
@@ -13,31 +19,61 @@ class CostFunction:
     @staticmethod
     def get_particle_cost(clew):
         cost_obj = CostFunction(clew)
-        total_cost = 0
+        particle_cost = 0
         for worm in clew:
-            total_cost += cost_obj.get_cost_worm_total(worm)
-        return total_cost
+            particle_cost += cost_obj.get_cost_worm_total(worm)
+        return particle_cost
 
     def get_cost_worm_total(self, worm):
         self._worm = worm
 
         # Normalise the cost ?
         internal_cost = CostFunction.w_i * self.get_internal_knowledge_cost()
-        group_cost = CostFunction.w_g * self.get_group_knowledge_cost()
-        external_cost = CostFunction.w_e * self.get_external_knowledge_cost()
-        total_cost = internal_cost + group_cost + external_cost
-        return total_cost
+
+        # group_cost = CostFunction.w_g * self.get_group_knowledge_cost()
+        # external_cost = CostFunction.w_e * self.get_external_knowledge_cost()
+
+        group_cost = 0
+        external_cost = 0
+        worm_cost = internal_cost + group_cost + external_cost
+        return worm_cost
 
     def get_internal_knowledge_cost(self):
-        #TODO
-        return -1
+        
+        # Better representation in case we need to normalize component values of each knowledge
+
+        component_1_cost = width_cost(self._worm, CostFunction.max_possible_width)
+
+        components_cost_arr = [component_1_cost]
+
+        return sum(components_cost_arr)
 
     def get_group_knowledge_cost(self):
-        #TODO
-        return -1
+
+        # needs to modularized
+        def intersect(a1, a2, b1, b2):
+            area = lambda p1, p2, p3 : (p2[0]-p1[0])*(p3[1]-p1[1])-(p3[0]-p1[0])*(p2[1]-p1[1])
+            v1 = area(a1, a2, b1)/1000
+            v2 = area(a1, a2, b2)/1000
+            return v1 * v2
+
+        min_cost = 10000
+        for worm2 in self.clew:
+            if self._worm == worm2: continue
+            points1 = self._worm.control_points()
+            points2 = worm2.control_points()
+
+            for i in range(len(points1)-1):
+                cost1 = intersect(points1[i], points1[i+1], points2[i], points2[i+1])
+                cost2 = intersect(points2[i], points2[i+1], points1[i], points1[i+1])
+                cost = max([cost1, cost2])
+                if cost < min_cost:
+                    min_cost = cost
+        if min_cost > 0:
+            return 0
+        else:
+            return 1
 
     def get_external_knowledge_cost(self):
         #TODO
         return -1
-
-
---
+++ internalcost.py
@@ -0,0 +1,5 @@
+def width_cost(worm, max_possible_width_worm):
+    return max_possible_width_worm - worm.width
+
+def straightness_cost(worm):
+    return -1
